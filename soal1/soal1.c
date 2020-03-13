994d26874de908d85b6685e4eb8e5269d2c06a99
zmahmudi <52489034+zmahmudi@users.noreply.github.com>
2020-03-13 17:11:41 +0700

    update soal1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -33,111 +33,112 @@ int main(int argc, char const *argv[]) {
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
   close(STDERR_FILENO);
+  while(1){
+    char min, sec, hour;
+    char *cmd[] = {"bash", argv[4], NULL};
 
-  char min, sec, hour;
-  char *cmd[] = {"bash", argv[4], NULL};
+      if(argc>=4){
+        printf("Error\n");
+        return 0;
+      }
+      if(atoi(argv[1])<0 || atoi(argv[1])>59 || argv[1] != "\\*"){
+        printf("Error\n");
+        return 0;
+      }
 
-    if(argc>=4){
-      printf("Error\n");
-      return 0;
-    }
-    if(atoi(argv[1])<0 || atoi(argv[1])>59 || argv[1] != "\\*"){
-      printf("Error\n");
-      return 0;
-    }
+      if(argv[1] == "\\*"){
+        sec = '*';
+      }
+      else{
+        sec = atoi(argv[1]);
+      }
+      
+      if(atoi(argv[2])<0 || atoi(argv[2])>59 || argv[2] != "\\*"){
+        printf("Error\n");
+        return 0;
+      }
+      if(argv[2] == "\\*"){
+        min = '*';
+      }
+      else{
+        min = atoi(argv[2]);
+      }
+      if(atoi(argv[3])<0 || atoi(argv[3])>23 || argv[3] != "\\*"){
+        printf("Error\n");
+        return 0;
+      }
+      if(argv[3] == "\\*"){
+        hour = '*';
+      }
+      else{
+        hour = atoi(argv[3]);
+      }
 
-    if(argv[1] == "\\*"){
-      sec = '*';
-    }
-    else{
-      sec = atoi(argv[1]);
-    }
-    
-    if(atoi(argv[2])<0 || atoi(argv[2])>59 || argv[2] != "\\*"){
-      printf("Error\n");
-      return 0;
-    }
-    if(argv[2] == "\\*"){
-      min = '*';
-    }
-    else{
-      min = atoi(argv[2]);
-    }
-    if(atoi(argv[3])<0 || atoi(argv[3])>23 || argv[3] != "\\*"){
-      printf("Error\n");
-      return 0;
-    }
-    if(argv[3] == "\\*"){
-      hour = '*';
-    }
-    else{
-      hour = atoi(argv[3]);
-    }
+    time_t now;
+    struct tm *now_tm;
+    int currh, currm, currs;
 
-  time_t now;
-  struct tm *now_tm;
-  int currh, currm, currs;
+    now = time(NULL);
+    now_tm = localtime(&now);
+    currh = now_tm->tm_hour;
+    currm = now_tm->tm_min;
+    currs = now_tm->tm_sec;
 
-  now = time(NULL);
-  now_tm = localtime(&now);
-  currh = now_tm->tm_hour;
-  currm = now_tm->tm_min;
-  currs = now_tm->tm_sec;
+    if(sec=='*'&&min!='*'&&hour!='*'){
+      while (hour==currh && min==currm) {
 
-  if(sec=='*'&&min!='*'&&hour!='*'){
-    while (hour==currh && min==currm) {
-
-    execv("/bin/bash",cmd);
-    
-    sleep(1);
+      execv("/bin/bash",cmd);
+      
+      sleep(1);
+      }
     }
-  }
-  else if(min=='*'&&sec!='*'&&hour!='*'){
-    while (hour==currh && sec==currs) {
+    else if(min=='*'&&sec!='*'&&hour!='*'){
+      while (hour==currh && sec==currs) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(60);
+      execv("/bin/bash",cmd);
+      
+      sleep(60);
+      }
     }
-  }
-  else if(hour=='*'&&sec!='*'&&min!='*'){
-    while (min==currm && sec==currs) {
+    else if(hour=='*'&&sec!='*'&&min!='*'){
+      while (min==currm && sec==currs) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(3600);
+      execv("/bin/bash",cmd);
+      
+      sleep(3600);
+      }
     }
-  }
-  else if(min=='*'&&sec=='*'&&hour!='*'){
-    while (hour==currh) {
+    else if(min=='*'&&sec=='*'&&hour!='*'){
+      while (hour==currh) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(1);
+      execv("/bin/bash",cmd);
+      
+      sleep(1);
+      }
     }
-  }
-  else if(min=='*'&&sec!='*'&&hour=='*'){
-    while (sec==currs) {
+    else if(min=='*'&&sec!='*'&&hour=='*'){
+      while (sec==currs) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(3660);
+      execv("/bin/bash",cmd);
+      
+      sleep(3660);
+      }
     }
-  }
-  else if(min!='*'&&sec=='*'&&hour=='*'){
-    while (min==currm) {
+    else if(min!='*'&&sec=='*'&&hour=='*'){
+      while (min==currm) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(3601);
+      execv("/bin/bash",cmd);
+      
+      sleep(3601);
+      }
     }
-  }
-  else if(min=='*'&&sec=='*'&&hour=='*'){
-    while (1) {
+    else if(min=='*'&&sec=='*'&&hour=='*'){
+      while (1) {
 
-    execv("/bin/bash",cmd);
-    
-    sleep(1);
+      execv("/bin/bash",cmd);
+      
+      sleep(1);
+      }
     }
   }
   return 0;
